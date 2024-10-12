69caa99b5f9ed1327e8c0578f68971c65e91afe8
Khaled Elbalal <khaledelbalal@gmail.com>
2024-10-12 12:17:23 +0300

    Created model for the backend

--- automl/models.py
+++ automl/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class ModelEntry(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    task = models.CharField(max_length=255)
+    target_variable = models.CharField(max_length=255)
+    list_of_features = models.TextField(blank=True)
+    status = models.CharField(max_length=255)
+    model_name = models.CharField(max_length=255)
+    evaluation_metric = models.CharField(max_length=255)
+    evaluation_metric_value = models.FloatField()
