df3a3613181c48a5a9ac79acdf60cf3fa26344e2
malaksherif03 <malak.abouelkhair@gmail.com>
2024-10-12 13:19:34 +0300

    implemnted feature engineering function

--- automl/utils/functions.py
+++ automl/utils/functions.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import numpy as np
 from pandas import DataFrame
+from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.base import ClassifierMixin
@@ -87,13 +89,66 @@ def preprocess(df: DataFrame) -> DataFrame:
     except Exception as e:
         print(f"Error in removing remaining null values: {e}")
 
+
     df_cleaned = df
     return df_cleaned
 
+
 # Feature Engineering function
 def featureEngineer(df: DataFrame, target_variable: str) -> DataFrame:
-    df_features = df
-    return df_features
+    # 1. Removing highly correlated features
+    try:
+        numerical_columns = df.select_dtypes(include=['int64', 'float64']).columns
+        corr_matrix = df[numerical_columns].corr().abs()
+
+        upper_tri = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k=1).astype(bool))
+
+        high_corr_pairs = [(col1, col2) for col1 in upper_tri.columns for col2 in upper_tri.index if
+                           upper_tri.loc[col2, col1] > 0.80]
+        columns_to_drop = []
+
+        for col1, col2 in high_corr_pairs:
+            # Get correlation with the target variable
+            corr_with_target_col1 = df[col1].corr(df[target_variable])
+            corr_with_target_col2 = df[col2].corr(df[target_variable])
+
+            # Drop the column with lower correlation to the target variable
+            if abs(corr_with_target_col1) < abs(corr_with_target_col2):
+                columns_to_drop.append(col1)
+            else:
+                columns_to_drop.append(col2)
+        df.drop(columns_to_drop, axis=1, inplace=True)
+    except Exception as e:
+        print(f"Error in creating the correlation matrix: {e}")
+
+    # 2. Applying PCA
+    try:
+        pca = PCA()
+        pca.fit(df.drop(target_variable, axis=1))
+
+        cumulative_variance = np.cumsum(pca.explained_variance_ratio_)
+        thresholds = [0.90, 0.95, 0.99]
+        best_components = []
+
+        for threshold in thresholds:
+            n_components = np.argmax(cumulative_variance >= threshold) + 1
+            best_components.append(n_components)
+
+        pca = PCA(n_components=best_components[0])
+        df_transformed = pca.fit_transform(df.drop(target_variable, axis=1))
+
+        # Convert the transformed array back to a DataFrame if needed
+        df_pca = pd.DataFrame(df_transformed, columns=[f'PC{i + 1}' for i in range(best_components[0])])
+
+        # Optionally concatenate the target variable back to the PCA DataFrame if needed
+        df_final = pd.concat([df_pca, df[target_variable].reset_index(drop=True)], axis=1)
+        return df_final
+
+    except Exception as e:
+       print(f"Error Applying PCA: {e}")
+
+    return df
+
 
 # Model Selection function
 def selectBestModel(df: DataFrame, target_variable_name: str) -> ClassifierMixin:
